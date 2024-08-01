bab9597187bc4348d7f7120f1d7e14effb10a12b
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-08-01 13:33:36 +0700

    реализованы тесты к модулю product

--- tests/test_product.py
+++ tests/test_product.py
@@ -23,3 +23,9 @@ def test_product_info_price(capsys, product):
     assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
     product.price = 100
     assert product.price == 100
+
+def test_product_str(product):
+    assert str(product) == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
+
+def test_product_add(product1, product2):
+    assert product1 + product2 == 2580000
