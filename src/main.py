064b1e730f67f06eb0bc2ed44cd882e99c5aa2b0
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-08-11 22:36:10 +0700

    отформатированы модули тестов

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 from src.category import Category
-from src.product import Product
-
-
-from src.smartphone import Smartphone
 from src.lawngrass import LawnGrass
-
+from src.product import Product
+from src.smartphone import Smartphone
 
 if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
